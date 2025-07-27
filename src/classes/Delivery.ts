6cec4242501607749e93a245b10eed4d12f35912
Андрей Евтухов <stalker10eg@mail.ru>
2025-07-27 19:09:29 +0300

    remake task 1

--- src/classes/Delivery.ts
+++ src/classes/Delivery.ts
@@ -1,18 +1,41 @@
 import { createSubtitleWithInfo } from '../utils'
 
 export class Delivery {
-  protected name: string
-  protected address: string
-  protected distance: number
+  private id: string = crypto.randomUUID()
+  private name: string
+  private address: string
+  private distance: number
 
   constructor (name: string, address: string, distance: number) {
     this.name = name
     this.address = address
     this.distance = distance
+    this.render()
+  }
+
+  render () {
+    const cardList = document.getElementById('card-list')
+    let card = cardList?.querySelector(`[data-id="${this.id}"]`) as HTMLElement | null
+
+    if (card) {
+      card.replaceChildren()
+    } else {
+      card = document.createElement('div')
+      card.dataset.id = this.id
+      card.className = 'delivery-card'
+      cardList?.append(card)
+    }
+
+    const nameElement = createSubtitleWithInfo('Имя', this.name)
+    const addressElement = createSubtitleWithInfo('Адрес', this.address)
+    const distanceElement = createSubtitleWithInfo('Расстояние', `${this.distance} км`)
+
+    card.append(nameElement, addressElement, distanceElement)
   }
 
   setName (value: string) {
     this.name = value
+    this.render()
   }
 
   getName () {
@@ -21,6 +44,7 @@ export class Delivery {
 
   setAddress (value: string) {
     this.address = value
+    this.render()
   }
 
   getAddress () {
@@ -29,22 +53,10 @@ export class Delivery {
 
   setDistance (value: number) {
     this.distance = value
+    this.render()
   }
 
   getDistance () {
     return this.distance
-  }
-
-  renderDeliveryCard () {
-    const deliveryCard = document.createElement('div')
-    deliveryCard.className = 'delivery-card'
-
-    const nameElement = createSubtitleWithInfo('Имя', this.name)
-    const addressElement = createSubtitleWithInfo('Адрес', this.address)
-    const distanceElement = createSubtitleWithInfo('Расстояние', `${this.distance} км`)
-
-    deliveryCard.append(nameElement, addressElement, distanceElement)
-
-    return deliveryCard
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,12 @@ import { Delivery } from './classes'
 
 import './style.css'
 
-const deliveryList: Delivery[] = [
+const deliveryList = [
   new Delivery('Ольга', 'ул. Вымыслов, д. 12', 8),
   new Delivery('Дмитрий', 'ул. Задачная, д. 7', 3),
-  new Delivery('Оля', 'ул. Ткачей, д. 43', 11)
+  new Delivery('Оля', 'ул. Ткачей, д. 43', 11),
 ]
 
-const app = document.querySelector('#app')
-const cardList = document.createElement('div')
-cardList.className = 'card-list'
+deliveryList.forEach(listItem => listItem.render())
 
-deliveryList.map((delivery) => cardList?.append(delivery.renderDeliveryCard()))
-
-app?.append(cardList)
+deliveryList[0].setName('Жора')
